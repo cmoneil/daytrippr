025f915de2c4995bdee4f798c235891ced1e117c
Conor O’Neil <conoroneil@stp-er-10-129-18-173.wireless.umn.edu>
2018-08-23 21:01:56 -0500

    Deploying to Heroku

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,6 @@
 export const AUTH_CONFIG = {
     domain: "cmoneil.auth0.com",
     clientId: 'JDu-znFsZJYYx9PMJlCGDBYt8PPRjCnl',
-    callbackUrl: 'http://localhost:3000/callback'
+    // callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'https://vast-fortress-86780.herokuapp.com/'
   }
