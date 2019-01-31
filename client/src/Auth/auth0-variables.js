ac22a345acfeeefcfd7048ecd32b6ef12551cf7d
Conor <conor@theconoroneil.com>
2019-01-30 20:40:48 -0600

    Small auth0 variable fix

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: "cmoneil.auth0.com",
-    lientId: process.env.REACT_AUTH0_VARIABLE,,
+    lientId: process.env.REACT_AUTH0_VARIABLE,
     // callbackUrl: 'http://localhost:3000/callback'
     callbackUrl: 'https://vast-fortress-86780.herokuapp.com/'
   }
