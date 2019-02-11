25d17bdcabd9072c7809e248ebb6122f495d47f9
Conor <conor@theconoroneil.com>
2019-02-10 19:08:13 -0600

    Auth0 variables

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,9 +1,8 @@
 export const AUTH_CONFIG = {
     domain: "cmoneil.auth0.com",
-    // clientId: `${process.env.REACT_APP_AUTH0_VARIABLE}`,
-    // callbackUrl: 'https://vast-fortress-86780.herokuapp.com/'
-    clientId: 'JDu-znFsZJYYx9PMJlCGDBYt8PPRjCnl',
-    callbackUrl: 'http://localhost:3000/callback'
+    clientId: `${process.env.REACT_APP_AUTH0_VARIABLE}`,
+    callbackUrl: 'https://vast-fortress-86780.herokuapp.com/'
+   
 
     
   }
